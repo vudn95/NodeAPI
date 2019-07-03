bfed6f504f9ce68fa7007682d3baaaf8cd2240c6
VienNguyen113 <luongvien13t2@gmail.com>
2019-07-03 11:28:49 +0700

    Fix CORS

--- api/controller/product.js
+++ api/controller/product.js
@@ -5,6 +5,7 @@ export const getProducts = (req, res) => {
     let sql = 'SELECT * FROM products'
     db.query(sql, (err, response) => {
         if (err) throw err
+        res.status(400);
         res.json(response)
     })
 }
