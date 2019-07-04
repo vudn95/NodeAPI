1f7c2ed270dc9fc1851484eaae2d07a84e7fbaf1
Quang <quetoiohue@gmail.com>
2019-07-04 10:32:38 +0700

    apiInsertByProcedure

--- api/controller/product.js
+++ api/controller/product.js
@@ -5,7 +5,17 @@ export const getProducts = (req, res) => {
     let sql = 'SELECT * FROM products'
     db.query(sql, (err, response) => {
         if (err) throw err
-        res.status(400);
+        // res.status(400);
+        res.json(response)
+    })
+}
+
+export const getLengthProducts = (req, res) => {
+    let sql = `SELECT COUNT(id)
+    FROM products`
+    db.query(sql, (err, response) => {
+        if (err) throw err
+        // res.status(400);
         res.json(response)
     })
 }
--- api/router.js
+++ api/router.js
@@ -1,7 +1,12 @@
-import {getProducts} from './controller/product';
+import { getProducts, getLengthProducts } from './controller/product';
+import { insertProducts } from './controller/insertProducts';
+
 const routes = (app) => {
     app.route('/product')
         .get(getProducts)
+
+    app.route('/product/length').get(getLengthProducts)
+    app.route('/product/insert/:name&:color&:price').get(insertProducts)
 }
  
 export default routes;
